3da4c21681074d308d879a357b0eba0e1f79e2d5
Lukas Heberling <lukaselia.heberling@gmail.com>
2024-03-11 22:31:54 +0100

    Validating the user input

--- src/players.h
+++ src/players.h
@@ -6,6 +6,12 @@ players_t *init_players();
 
 void copy_players(players_t *dest, players_t *players);
 
+//! deprecated
 void free_players(players_t *players);
+
+char validate_action(
+    block_t **map,
+    player_t *player,
+    player_action_t player_action);
 
 #endif
