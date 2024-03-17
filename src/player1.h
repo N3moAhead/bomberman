1ca14d6bbb60ccc679f091bc7730fc315ca03b6b
unknown <lukaselia.heberling@gmail.com>
2024-03-17 18:11:10 +0100

    Added the current game round to the players

--- src/player1.h
+++ src/player1.h
@@ -2,6 +2,6 @@
 #define PLAYER_1_H
 #include "types.h"
 
-player_action_t get_player_1_action(block_t **map, players_t *players);
+player_action_t get_player_1_action(block_t **map, players_t *players, int game_round);
 
 #endif
