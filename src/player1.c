229f7fbe9ac85099ddff427735d8dca6860e8be5
Lukas Heberling <lukaselia.heberling@gmail.com>
2024-04-05 14:51:14 +0200

    Bombs: Update bombs just every second round
    Bombs exploded way to fast so now they are only getting udpated every
    second turn. Im currently not to sure if this is a good change, but it
    can hang around like that for a little bit.

--- src/player1.c
+++ src/player1.c
@@ -3,5 +3,6 @@
 
 // This function gets called on each tick
 player_action_t get_player_1_action(block_t **map, players_t *players, int game_round) {
-  return NONE;
+  player_action_t actions[3] = {PLANT_BOMB, MOVE_DOWN, MOVE_RIGHT};
+  return actions[game_round % 3];
 }
