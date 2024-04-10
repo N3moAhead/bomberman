bdf646eb445aa99f243f92ef2a4f5f03de0a89ca
Lukas H <62028575+N3moAhead@users.noreply.github.com>
2024-04-10 13:31:14 +0200

    init_map: Changed the wall placement

--- src/constants.h
+++ src/constants.h
@@ -1,6 +1,6 @@
 // Map Settings
-#define MAP_HEIGHT 11
-#define MAP_WIDTH 11
+#define MAP_HEIGHT 11 // should be odd
+#define MAP_WIDTH 11 // should be odd
 
 // Player Settings
 #define MAX_LIVES 3
