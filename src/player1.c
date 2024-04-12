bfe389b5472615775b648e8bf83e2a5c8ace57bc
Lukas H <62028575+N3moAhead@users.noreply.github.com>
2024-04-12 13:37:18 +0200

    Its now possible to give names to a written bots
    I thought it would be way more fun if authors would be able to see who
    they are going to play against! So this is what has come out of it XD

--- src/player1.c
+++ src/player1.c
@@ -2,9 +2,15 @@
 #include "types.h"
 #include "constants.h"
 #include "bots/n3mo_bot_v1.h"
+#include <string.h>
 
 // This function gets called on each tick
 player_action_t get_player_1_action(block_t **map, players_t *players, int game_round)
 {
   return get_bot_move(map, players, game_round, players->player1);
+}
+
+void get_player1_bot_description(char *bot_name)
+{
+  get_bot_description(bot_name);
 }
