18cf243cb2066c39706145435efec91899895081
Lukas H <62028575+N3moAhead@users.noreply.github.com>
2024-04-14 17:57:56 +0200

    Adding new SDL constants

--- src/constants.h
+++ src/constants.h
@@ -4,3 +4,13 @@
 
 // Player Settings
 #define MAX_LIVES 3
+
+/** SDL SETTINGS **/
+// Window size zoom
+#define WINDOW_ZOOM 3
+// The size of the source sprites
+#define ASSET_SPRITE_SIZE 32
+// The size of the sprites to print to the screen
+#define DISPLAY_SPRITE_SIZE (ASSET_SPRITE_SIZE * WINDOW_ZOOM)
+#define WINDOW_WIDTH (MAP_WIDTH * DISPLAY_SPRITE_SIZE)
+#define WINDOW_HEIGHT (MAP_HEIGHT * DISPLAY_SPRITE_SIZE)
