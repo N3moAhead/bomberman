3dcd8a9d71731c420363b9a7db0c883eb5cae4d9
Lukas H <62028575+N3moAhead@users.noreply.github.com>
2024-04-14 17:58:45 +0200

    Adding new types and global variables

---
+++ src/globals.h
@@ -0,0 +1,11 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+#include <SDL2/SDL.h>
+#include "types.h"
+
+extern SDL_Window *window;
+extern SDL_Renderer *renderer;
+extern SDL_Texture *texture_atlas;
+extern atlas_positions_t texture_atlas_positions;
+
+#endif
