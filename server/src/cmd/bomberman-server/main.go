94466e811da0d5fa12886a9b92785b3fda4ce389
Lukas H <62028575+N3moAhead@users.noreply.github.com>
2025-01-11 19:01:45 +0100

    feat: Implemented a pingPong upd server

--- server/src/cmd/bomberman-server/main.go
+++ server/src/cmd/bomberman-server/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
+
+func panicOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello I'm the Bomberman Server XD")
+	addr, err := net.ResolveUDPAddr("udp", ":8080")
+	panicOnError(err)
+
+	conn, err := net.ListenUDP("udp", addr)
+	panicOnError(err)
+
+	fmt.Println("Bomberman-Server is running on Port 8080...")
+
+	buffer := make([]byte, 1024)
+	for {
+		n, clientAddr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		fmt.Printf("Empfangen von %s: %s\n", clientAddr, string(buffer[:n]))
+
+		_, err = conn.WriteToUDP([]byte("Message received!"), clientAddr)
+		if err != nil {
+			fmt.Println("Error while answering:", err)
+		}
+	}
 }
