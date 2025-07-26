b06f79ab69aafebc7666033ed05c24c8dcee6279
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2025-07-26 18:50:49 +0200

    feat(server): Added websocket utils
    Those are files from an older project i did with Driemtax.
    Its called Archaide and had been a multiplayer Arcade game ;)
    If you like my stuff... a visit to his github is definitly worth it!
    http://github.com/Driemtax

--- server/src/go.mod
+++ server/src/go.mod
@@ -1,4 +1,4 @@
-module github.com/N3moAhead/bomberman
+module github.com/N3moAhead/bomberman/server
 
 go 1.24.5
 
---
+++ server/src/internal/server/server.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/N3moAhead/bomberman/server/internal/hub"
+)
+
+func Run(addr *string) {
+	hubInstance := hub.NewHub()
+
+	go hubInstance.Run()
+
+	// Register the WebSocket handler
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		// Pass the single hub instance to the handler
+		serveWs(hubInstance, w, r)
+	})
+
+	// Simple handler for the root path
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("[BOMBERMAN-SERVER] is running. Connect via WebSocket on /ws"))
+	})
+
+	log.Printf("Server starting on %s", *addr)
+	// Start the HTTP server
+	err := http.ListenAndServe(*addr, nil) // Use the default ServeMux
+	if err != nil {
+		log.Fatalf("ListenAndServe failed: %v", err)
+	}
+}
